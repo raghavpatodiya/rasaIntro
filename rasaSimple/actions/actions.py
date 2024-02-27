c27c37b5b82a94d507192118b11b99ce0a83a969
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-02-27 11:14:44 +0530

    Testing Actions

--- rasaSimple/actions/actions.py
+++ rasaSimple/actions/actions.py
@@ -1,27 +1,31 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
-#
+
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ExtractFoodEntity(Action):
+
+    def name(self) -> Text:
+        return "action_extract_food_entity"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        food_entity = next(tracker.get_latest_entity_values('food'), None)
+        if food_entity:
+            dispatcher.utter_message(text=f"you have selected {food_entity} as your food choice")
+        else:
+            dispatcher.utter_message(text=" i am sorry, i could not detect the food choice")
+        return []
+    
+# enable action endpoints in endpoints.yml

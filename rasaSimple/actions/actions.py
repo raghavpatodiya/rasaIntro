c7511106c0bafd2d20b6bd6e17cb0f57439f97e6
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-02-27 12:08:25 +0530

    Adding Stories

--- rasaSimple/actions/actions.py
+++ rasaSimple/actions/actions.py
@@ -28,4 +28,25 @@ class ExtractFoodEntity(Action):
             dispatcher.utter_message(text=" i am sorry, i could not detect the food choice")
         return []
     
+class OtherFoodAction(Action):
+    def name(self)->Text:
+        return "action_order_food"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Sure, which kind of food would you like to order")
+        return []
+
+class ConfirmOrderAction(Action):
+    def name(self)->Text:
+        return "action_confirm_order"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        food_entity = next(tracker.get_latest_entity_values('food'), None)
+        if food_entity:
+            dispatcher.utter_message(text=f"i have ordered {food_entity} for you")
+        else:
+            dispatcher.utter_message(text=" i am sorry, i could not detect the food choice")
+        return []  
 # enable action endpoints in endpoints.yml

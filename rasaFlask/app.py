25fc5e0658b3e15b17a64680df2e53d6bf96cf74
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-02-27 13:53:27 +0530

    Error Related to port:5005

--- rasaFlask/app.py
+++ rasaFlask/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-# used to store url's of rasa's servers
-RASA_API_URL = 'http://localhost:5005/webhooks/rest/webhook'
 
+# URL of Rasa's server
+RASA_API_URL = 'http://localhost:5005/webhooks/rest/webhook'
 
 app = Flask(__name__)
 
@@ -10,21 +10,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# webhook route
+# Webhook route
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    user_message=request.json['message']
-    print("User message:", user_message)
+    try:
+        user_message = request.json.get('message', '')  # Safely get the message from JSON data
+        print("User message:", user_message)
 
-    # end users message to RASA and get bot's response
-    rasa_response = requests.post(RASA_API_URL, json={'message':user_message})
-    rasa_response_json =  rasa_response.json()
+        # Send the user's message to RASA and get the bot's response
+        rasa_response = requests.post(RASA_API_URL, json={'message': user_message})
+        rasa_response_json = rasa_response.json()
 
-    print("Rasa Response:", rasa_response_json)
+        print("Rasa Response:", rasa_response_json)
 
-    bot_response = rasa_response_json[0]['text'] if rasa_response_json else 'Sorry, I didn\'t  understand that.'
+        bot_response = rasa_response_json[0]['text'] if rasa_response_json else 'Sorry, I didn\'t understand that.'
+    
+    except Exception as e:
+        # Handle exceptions gracefully
+        print("An error occurred:", e)
+        bot_response = 'An error occurred while processing your request.'
 
     return jsonify({'response': bot_response})
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True, port=3000)
